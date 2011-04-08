f72a2421263edde117a73fc7ad11c1aefb9b915a
James Hughes <hughes.james@gmail.com>
2011-04-08 18:13:01 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Randomfma::Application.config.secret_token = 'ef788478615133b10e0a26b2b36404cf517ed719ca1448aaa31ea08b083fc0b9e988884946abb2ce1a01e55e6468f7dc4fd26babbec79441d8c8a181ba56a80d'
