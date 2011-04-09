35801ca9e1156671f0d30c5564536ebf62121d7d
James Hughes <hughes.james@gmail.com>
2011-04-09 19:28:48 -0400

    Nokogiri

--- app/controllers/fma_controller.rb
+++ app/controllers/fma_controller.rb
@@ -1,6 +1,8 @@
-#require 'curb.rb'
 class FmaController < ApplicationController
   def index
-    @c = Curl::Easy.perform("http://freemusicarchive.org/api/get/curators.xml")   
+    xml_str = Curl::Easy.perform("http://freemusicarchive.org/api/get/curators.xml").body_str
+    xml_doc = Nokogiri::XML(xml_str)
+    @curator_names = xml_doc.xpath("//curator_handle")
+    
   end
 end
