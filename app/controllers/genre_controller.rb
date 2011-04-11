2b02adc2dde249be03126ff29ff44c339954eca3
James Hughes <hughes.james@gmail.com>
2011-04-11 07:57:00 -0400

    Genre controller

---
+++ app/controllers/genre_controller.rb
@@ -0,0 +1,18 @@
+class GenreController < ApplicationController
+Genres = ["Blues",
+"Classical",
+"Country",
+"Electronic",
+"Experimental",
+"Folk",
+"Hip-Hop",
+"International",
+"Jazz",
+"Novelty",
+"Old-Time / Historic",
+"Pop",
+"Rock",
+"Soul-R&B",
+"Spoken"]
+ 
+end
