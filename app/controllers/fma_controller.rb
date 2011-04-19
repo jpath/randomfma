874efad952febee019f66f444334859d4d724e77
James Hughes <hughes.james@gmail.com>
2011-04-19 10:19:13 -0400

    Removing unused view, controller.

--- app/controllers/fma_controller.rb
+++
@@ -1,8 +0,0 @@
-class FmaController < ApplicationController
-  def index
-    xml_str = Curl::Easy.perform("http://freemusicarchive.org/api/get/curators.xml").body_str
-    xml_doc = Nokogiri::XML(xml_str)
-    @curator_names = xml_doc.xpath("//curator_handle")
-    
-  end
-end
