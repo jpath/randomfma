6924e932bd170bf73a678ab9739015788a760f54
James Hughes <hughes.james@gmail.com>
2011-04-20 16:46:26 -0400

    CSS changes, add album and artist to playlist page.

--- test/functional/playlists_controller_test.rb
+++ test/functional/playlists_controller_test.rb
@@ -4,7 +4,10 @@ class PlaylistsControllerTest < ActionController::TestCase
   def setup 
     @pl = Playlist.new :genre_handle => "Rock", :limit => 1
     @pl.tracks << Track.new(:url => "http://fma.org/track1", 
-      :title => "Track 1", :fma_id => "1")
+      :title => "Track 1", :fma_id => "1", 
+      :album => "album1", :artist => "artist1",
+      :artist_url => "http://fma.org/artist1",
+      :album_url => "http://fma.org/album1")
     @pl.id = 1
     stub(Playlist).find(1) {@pl}
   end
@@ -23,10 +26,23 @@ class PlaylistsControllerTest < ActionController::TestCase
     assert_select "a[href=#{@pl.tracks.first.url}]" 
   end
 
+  test "show action displays link to album" do
+    get :show, :id => 1
+    assert_select "a", @pl.tracks.first.album
+    assert_select "a[href=#{@pl.tracks.first.album_url}]" 
+  end
+
+  test "show action displays link to artist" do
+    get :show, :id => 1
+    assert_select "a", @pl.tracks.first.artist
+    assert_select "a[href=#{@pl.tracks.first.artist_url}]" 
+  end
+
   test "links in list have an id" do 
     get :show, :id => 1
     assert_select "a[id=track-link-1]" 
   end
+
   test "links in list have a class attribute" do 
     get :show, :id => 1
     assert_select "a[class=track-link]" 
